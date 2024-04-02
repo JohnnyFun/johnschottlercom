c6d205032ea7ddee68365cd761cbd36c590da736
John Schottler <jcschottler1991@gmail.com>
2024-04-02 13:51:02 -0700

    wording tweak

--- src/components/AboutMe.svelte
+++ src/components/AboutMe.svelte
@@ -4,7 +4,7 @@
   </div>
   <div class="bio">
     <p>
-      Hi, I've been a full-stack web developer since 2010. I also dabble in video games and machine learning. Checkout my <a href="/resume">résumé</a> and <a href="/projects">hobby projects</a> for more about that.
+      Hi, I've been a full-stack web developer since 2010. I also dabble in creating video games and machine learning models. Checkout my <a href="/resume">résumé</a> and <a href="/projects">hobby projects</a> for more about that.
     </p>
     <p>
       In my personal time, I enjoy a variety of things. Currently, I'm primarily focused on learning jiu jitsu and spanish. I'm a blue belt in jiu jitsu and compete a few times per year for fun. I have a B1 level in spanish, and my goal is to achieve at least B2.
